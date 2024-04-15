fc57cfb3c7b0b820ba7191bf9f09a76273f49eb6
dhdorr <dhdorr@gmail.com>
2024-04-15 07:01:38 -0700

    cool

--- assignment_3/ClothSystem.cpp
+++ assignment_3/ClothSystem.cpp
@@ -4,7 +4,7 @@
 //TODO: Initialize here
 ClothSystem::ClothSystem()
 {
-	m_numParticles = 4;
+	m_numParticles = 6;
 	vector<Vector3f> temp_vecs;
 
 	// fill in code for initializing the state based on the number of particles
@@ -26,20 +26,16 @@ vector<Vector3f> ClothSystem::evalF(vector<Vector3f> state)
 
 	float mass = 0.01f;
 	float gravity_acceleration = -1.0f * 9.8f;
-	float drag_constant = 0.1f;
+	float drag_constant = 1.5f;
 	float spring_constant = 100.0f;
-	float spring_dampening = 0.01f;
-	float rest_length = 0.5f;
+	float spring_dampening = 0.1f;
+	float rest_length = 0.25f;
 
 	for (int s = 0; s < state.size(); s += 2) {
 		Vector3f prev_state = Vector3f(0);
 		if (s > 0) {
 			prev_state = state[s - 2];
 		}
-
-		// cout << "STATE: " << s << endl;
-		// state[s].print();
-		// state[s + 1].print();
 
 		float force_y = 0;
 		float force_x = 0;
@@ -54,6 +50,15 @@ vector<Vector3f> ClothSystem::evalF(vector<Vector3f> state)
 		// SPRING FORCE >:(
 		force_y += 1.0f * spring_constant * ((prev_state[1] - state[s][1]) - rest_length) * spring_dampening;
 		force_x += 1.0f * spring_constant * ((prev_state[0] - state[s][0])) * spring_dampening; 
+
+		// Calculate force to 2nd anchor
+		if (s >= state.size() - 2) {
+			force_y += -1.0f * spring_constant * ((state[s][1] - 0) - rest_length) * spring_dampening;
+			force_x += -1.0f * spring_constant * ((state[s][0] - 2.0f)) * spring_dampening; 
+		} else {
+			force_y += 1.0f * spring_constant * ((state[s][1] - state[s][1]) - rest_length) * spring_dampening;
+			force_x += 1.0f * spring_constant * ((state[s][0] - state[s][0])) * spring_dampening; 
+		}
 	
 
 		// push back velocity then sum of forces
@@ -69,12 +74,8 @@ vector<Vector3f> ClothSystem::evalF(vector<Vector3f> state)
 void ClothSystem::draw()
 {
 	vector<Vector3f> cloth_particles = this->getState();
-	// cout << "draw vecs: " << cloth_particles.size() << endl;
 
 	for (int p = 0; p < cloth_particles.size(); p += 2) {
-		cout << "\nDraw STATE: " << p << endl;
-		cloth_particles[p].print();
-		cloth_particles[p + 1].print();
 		//  position of particle i.
 		glPushMatrix();
 		glTranslatef(cloth_particles[p][0], cloth_particles[p][1], cloth_particles[p][2]);
@@ -102,6 +103,27 @@ void ClothSystem::draw()
 		glScalef(1, len / size, 1);
 		glutSolidCube(size);
 		glPopMatrix();
+
+		// Calculate spring to 2nd anchor
+		if (p >= cloth_particles.size() - 2) {
+
+			Vector3f spring_position = Vector3f((cloth_particles[p][0] + 2.0f) / 2, (cloth_particles[p][1]) / 2, 0);
+
+			float dx = cloth_particles[p][0] - 2.0f;
+			float dy = cloth_particles[p][1];
+
+			float len = sqrt(pow(dx, 2) + pow(dy, 2));
+			float size = 0.1f;
+			float radians = atan2(dx, dy);
+			float degrees = -1 * radians * 180.0f / M_PI;
+
+			glPushMatrix();
+			glTranslatef(spring_position[0], spring_position[1], spring_position[2]);
+			glRotatef(degrees, 0, 0, 1);
+			glScalef(1, len / size, 1);
+			glutSolidCube(size);
+			glPopMatrix();
+		}
 	}
 
 
